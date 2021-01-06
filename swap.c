54dd85374abc947bde316fd1a8751cac3f30a984
SaloniYerawar15 <77059802+SaloniYerawar15@users.noreply.github.com>
2021-01-06 19:36:49 +0530

    Create swap.c
    swapping of numbers without temp

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the value of a:");
+	scanf("%d",&a);
+	printf("enter the value of b:");
+	scanf("%d",&b);
+	printf("Before swap a=%d b=%d",a,b);
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	printf("\n\nAfter swap a=%d b=%d",a,b);
+	return 0;
+	
+}
